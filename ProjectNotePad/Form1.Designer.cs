9fc79170f6def227b37eba31f9613cf315d5e573
Jonatas Nascimento <jonatas.nascimento@elisbrasil.onmicrosoft.com>
2022-08-05 10:18:30 -0300

    Add project files.

---
+++ ProjectNotePad/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace ProjectNotePad
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.tbxFilial = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxData = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxHora = new System.Windows.Forms.TextBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnReload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(592, 429);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnExport
+            // 
+            this.btnExport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExport.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExport.Location = new System.Drawing.Point(12, 500);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(152, 44);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpen.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnOpen.Location = new System.Drawing.Point(170, 500);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(155, 44);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // tbxFilial
+            // 
+            this.tbxFilial.BackColor = System.Drawing.Color.White;
+            this.tbxFilial.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxFilial.Location = new System.Drawing.Point(12, 36);
+            this.tbxFilial.Name = "tbxFilial";
+            this.tbxFilial.Size = new System.Drawing.Size(100, 23);
+            this.tbxFilial.TabIndex = 3;
+            this.tbxFilial.TextChanged += new System.EventHandler(this.tbxFilial_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Filial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(120, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Data";
+            // 
+            // tbxData
+            // 
+            this.tbxData.BackColor = System.Drawing.Color.White;
+            this.tbxData.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxData.Location = new System.Drawing.Point(119, 36);
+            this.tbxData.Name = "tbxData";
+            this.tbxData.Size = new System.Drawing.Size(141, 23);
+            this.tbxData.TabIndex = 5;
+            this.tbxData.TextChanged += new System.EventHandler(this.tbxData_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(269, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Hora";
+            // 
+            // tbxHora
+            // 
+            this.tbxHora.BackColor = System.Drawing.Color.White;
+            this.tbxHora.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbxHora.Location = new System.Drawing.Point(268, 36);
+            this.tbxHora.Name = "tbxHora";
+            this.tbxHora.Size = new System.Drawing.Size(141, 23);
+            this.tbxHora.TabIndex = 7;
+            this.tbxHora.TextChanged += new System.EventHandler(this.tbxHora_TextChanged);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPath.Location = new System.Drawing.Point(12, 547);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(36, 17);
+            this.lblPath.TabIndex = 9;
+            this.lblPath.Text = "path";
+            // 
+            // btnReload
+            // 
+            this.btnReload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReload.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnReload.Location = new System.Drawing.Point(331, 500);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(154, 44);
+            this.btnReload.TabIndex = 10;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(616, 598);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxHora);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxFilial);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox tbxFilial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxData;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxHora;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnReload;
+    }
+}
+
---
+++ ProjectNotePad/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectNotePad
+{
+    public partial class Form1 : Form
+    {
+        public string path { get; set; }
+        FileInfo file;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class offline
+        {
+            public string filial { get; set; }
+            public string codbar { get; set; }
+            public string data { get; set; }
+            public string hora { get; set; }
+        }
+
+        List<offline> ret;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadTxt();
+        }
+
+        private void ReadTxt()
+        {
+            if (!string.IsNullOrEmpty(path))
+            {
+                using (StreamReader txt = new StreamReader(path))
+                {
+                    string line;
+                    List<offline> codbar = new List<offline>();
+
+                    while ((line = txt.ReadLine()) != null)
+                    {
+                        codbar.Add(new offline() { filial = tbxFilial.Text, codbar = line.ToString(), data = tbxData.Text, hora = tbxHora.Text });
+                    }
+
+                    dataGridView1.DataSource = codbar;
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenTxt();
+        }
+
+        private void tbxFilial_TextChanged(object sender, EventArgs e)
+        {
+            ReadTxt();
+        }
+
+        private void tbxData_TextChanged(object sender, EventArgs e)
+        {
+            ReadTxt();
+        }
+
+        private void tbxHora_TextChanged(object sender, EventArgs e)
+        {
+            ReadTxt();
+        }
+        public void OpenTxt()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "NotePad|*.txt" })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //txtFilename.Text = openFileDialog.FileName;
+                    path = openFileDialog.FileName;
+                    lblPath.Text = openFileDialog.FileName;
+                    //path = Path.GetTempFileName();
+                    //var fi1 = new FileInfo(path);
+                    ReadTxt();
+                }
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(path))
+            {
+                MessageBox.Show("Selecione um txt");
+                return;
+            }
+            TextWriter writer = new StreamWriter($"{path}_Exported.txt");
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    if (j != 3)
+                    {
+                        writer.Write($"{dataGridView1.Rows[i].Cells[j].Value.ToString()},");
+                    }
+                    else
+                    {
+                        writer.Write($"{dataGridView1.Rows[i].Cells[j].Value.ToString()}");
+                    }
+                }
+                writer.Write("\r\n");
+            }
+            writer.Close();
+            MessageBox.Show("TXT Exported");
+
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            ReadTxt();
+        }
+    }
+}
